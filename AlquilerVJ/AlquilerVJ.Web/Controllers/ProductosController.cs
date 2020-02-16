107d97d9e360358c64259d38070e740ac3682a85
DELL <DELL@DESKTOP-3VDFBCI>
2020-02-16 17:46:46 -0600

    Agregamos un lista de Productos

---
+++ AlquilerVJ/AlquilerVJ.Web/Controllers/ProductosController.cs
@@ -0,0 +1,35 @@
+﻿using AlquilerVJ.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AlquilerVJ.Web.Controllers
+{
+    public class ProductosController : Controller
+    {
+        // GET: Productos
+        public ActionResult Index()
+        {
+            var producto1 = new ProductoModel();
+            producto1.Id = 1;
+            producto1.Descripcion = "Mortal Kombat 9";
+
+            var producto2 = new ProductoModel();
+            producto2.Id = 2;
+            producto2.Descripcion = "Fifa 2020";
+
+            var producto3 = new ProductoModel();
+            producto3.Id = 3;
+            producto3.Descripcion = "Resident Evil";
+
+            var listadeProductos =new List<ProductoModel>();
+            listadeProductos.Add(producto1);
+            listadeProductos.Add(producto2);
+            listadeProductos.Add(producto3);
+
+            return View(listadeProductos);
+        }
+    }
+}
---
+++ AlquilerVJ/AlquilerVJ.Web/Models/ProductoModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AlquilerVJ.Web.Models
+{
+    public class ProductoModel
+    {
+
+        public int Id { get; set; }
+        public string Descripcion { get; set; }
+
+    }
+}
